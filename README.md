2a8277cb48493f1cfbfc9b1f2170e41f3755aec7
Brendan <brendansleung@gmail.com>
2022-08-22 14:13:05 -0700

    initial commit of guessing game

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # num-guessing-game
- A simple Python game where you guess the number that the computer generates.
+ A simple Python game where you guess the integer that the computer generates. Best used in terminal.
+
+ The game will tell you if the number you guess is too high or low. 
---
+++ numguess.py
@@ -0,0 +1,25 @@
+import random
+
+print("Welcome to the guessing game!")
+
+bound1 = input("Enter the LEFT BOUND (smallest int that can be generated): ")
+bound2 = input("Enter the RIGHT BOUND (largest int that can be generated): ")
+
+print("\nGenerating random number...\n")
+
+numToGuess = random.randrange(int(bound1), int(bound2+1))  # generates random number
+
+print("Start guessing.\n")
+
+guess = 0  # start at 0 guesses
+guessedNum = None  # the guessed num that will input
+
+while (guessedNum != numToGuess) :
+    guess = guess + 1
+    guessedNum = input("Guess " + str(guess) + " : ")
+    if (guessedNum > numToGuess) :
+        print("Too high. Try again.")
+    if (guessedNum < numToGuess) :
+        print("Too low. Try again.")
+    if (guessedNum == numToGuess) :
+        print("You got the number right in " + str(guess) + " tries!!")
