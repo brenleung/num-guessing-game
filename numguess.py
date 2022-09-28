9d3e7bebbc32c2aa8edc483b81816ef418221979
Brendan <102711163+brenleung@users.noreply.github.com>
2022-09-28 12:29:19 -0700

    bug fixes

--- numguess.py
+++ numguess.py
@@ -7,7 +7,7 @@ bound2 = input("Enter the RIGHT BOUND (largest int that can be generated): ")
 
 print("\nGenerating random number...\n")
 
-numToGuess = random.randrange(int(bound1), int(bound2+1))  # generates random number
+numToGuess = random.randrange(int(bound1), int(bound2)+1)  # generates random number
 
 print("Start guessing.\n")
 
@@ -16,7 +16,7 @@ guessedNum = None  # the guessed num that will input
 
 while (guessedNum != numToGuess) :
     guess = guess + 1
-    guessedNum = input("Guess " + str(guess) + " : ")
+    guessedNum = int(input("Guess " + str(guess) + " : "))
     if (guessedNum > numToGuess) :
         print("Too high. Try again.")
     if (guessedNum < numToGuess) :
